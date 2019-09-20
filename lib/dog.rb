a86a7f75406fabf6794fa7c3def68d71d82ef6fd
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-09-20 01:52:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,15 @@
 class Dog 
   
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
+  def name = (dog_name)
+    @name = dog_name
   end
   
   def name
     @name
+  end
+  
+  def breed = (dog_breed)
+    @breed = dog_breed
   end
   
   def breed
